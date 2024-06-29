d90191e9b93fe0933146b1d4018b3e03391a9433
Krishna Vakte <kvakte02@gmail.com>
2024-06-29 19:03:23 +0530

    version 1

---
+++ backend/models/orderModel.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true 
+    },
+    items: {
+        type: Array,
+        required: true
+    },
+    amount: {
+        type: Number, 
+        required: true 
+    },
+    address: {
+        type: Object,
+        required: true 
+    },
+    status: {
+        type: String,
+        default: "Pending"
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    endTime: {
+        type: Date,
+        required:true,
+    },
+    payment: {
+        type: Boolean,
+        default: false 
+    }
+})
+
+const orderModel = mongoose.model.order || mongoose.model("order", orderSchema);
+
+export default orderModel;
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+      <div className='header'>
+          <div className="header-contents">
+              <h2>Discover and book your ideal sports facility here!</h2>
+              <p>Explore a wide range of top-notch sports facilities designed with state-of-the-art features and expert craftsmanship. Our mission is to meet your sporting needs and enhance your experience, one exceptional session at a time</p>
+              <button>Browse Facilities</button>
+          </div>
+      
+    </div>
+  )
+}
+
+export default Header
